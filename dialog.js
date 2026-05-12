879734609b423fd7cfd12e2fa532e87aac74459c
Bipan Lal <bipan.lal@signatureclinic.co.uk>
2026-05-12 14:51:24 +0100

    Improve external warning UI

--- dialog.js
+++ dialog.js
@@ -1,3 +1,31 @@
-function checkRecipients() {
-  alert("Recipients checked successfully.");
+const externalRecipients = [
+  "external.user@gmail.com",
+  "client@yahoo.com",
+  "partner@anothercompany.com"
+];
+
+window.onload = function () {
+
+  const emailList = document.getElementById("emailList");
+
+  externalRecipients.forEach(email => {
+
+    const div = document.createElement("div");
+
+    div.className = "email-item";
+
+    div.textContent = email;
+
+    emailList.appendChild(div);
+  });
+};
+
+function cancelSend() {
+
+  alert("Email sending cancelled.");
+}
+
+function sendAnyway() {
+
+  alert("Email sent successfully.");
 }
